8b993a8b9804da10c37c12f72f497011961f3c8c
Gustavo <gustavobarboza2003@gmail.com>
2023-10-16 19:25:02 -0300

    Organizing the project structure

--- src/server.js
+++ src/server.js
@@ -1,21 +1,12 @@
 const express = require("express");
 
+const routes = require("./routes")
+
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
+app.use(routes)
 
-  response.send(
-    `Id of message: ${id}.
-     user: ${user}`
-  );
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Pagina: ${page}. Mostrar: ${limit}`);
-});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
