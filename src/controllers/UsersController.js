dbb3647451b8419ddc6caa41c48b42f39fc993f7
Gustavo <gustavobarboza2003@gmail.com>
2023-10-24 21:02:44 -0300

    SELECT to find out if an email already exists in the database.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,21 @@
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
+
+const sqliteConnection = require("../database/sqlite");
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if(!name){
-      throw new AppError("O nome é obrigatório!")
-    }
+    const database = await sqliteConnection();
 
-    response.json({ name, email, password });
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+    return response.status(201).json();
   }
 }
 
