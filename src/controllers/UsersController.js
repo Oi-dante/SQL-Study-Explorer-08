c89b3bacf3c16c17193fc45f079807928ef5ec34
Gustavo <gustavobarboza2003@gmail.com>
2023-10-24 21:54:17 -0300

    Installed BCRYPTJS and created password security

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require("../database/sqlite");
 class UsersController {
   async create(request, response) {
@@ -15,9 +15,12 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso.");
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
-      [ name, email, password ])
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    );
 
     return response.status(201).json();
   }
