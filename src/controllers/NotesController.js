6ec9d8d08872f09bed8ab4ab8456c72f58a31537
Gustavo <gustavobarboza2003@gmail.com>
2023-10-31 20:41:19 -0300

    Notes list route

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -38,7 +38,9 @@ class NotesController {
 
     const note = await knex("notes").where({ id }).first();
     const tags = await knex("tags").where({ note_id: id }).orderBy("name");
-    const links = await knex("links").where({ note_id: id }).orderBy("created_at");
+    const links = await knex("links")
+      .where({ note_id: id })
+      .orderBy("created_at");
 
     return response.json({
       ...note,
@@ -47,12 +49,19 @@ class NotesController {
     });
   }
 
-  async delete(request, response){
+  async delete(request, response) {
     const { id } = request.params;
 
     await knex("notes").where({ id }).delete();
 
     return response.json();
+  }
+
+  async index(request, response) {
+    const { user_id } = request.query;
+    const notes = await knex("notes").where({ user_id }).orderBy("title");
+
+    return response.json({ notes });
   }
 }
 
